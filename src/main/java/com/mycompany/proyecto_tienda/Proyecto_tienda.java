1be2f6b769a5f38d9d94f1e95337077173a13ddb
Martin <martin.cardozo500@gmail.com>
2023-11-03 14:42:37 -0300

    Primer commit caretas

---
+++ src/main/java/com/mycompany/proyecto_tienda/Proyecto_tienda.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_tienda;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Martin Cardozo
+ */
+public class Proyecto_tienda {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Este es un mensaje de información.");
+
+    }
+}
